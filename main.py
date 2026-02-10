4dc9445121c26cedab72ec718538dcf3f7855ba4
lmraine <lraine@fhu.edu>
2026-02-10 14:49:36 -0600

    take 2 merge conflict

--- main.py
+++ main.py
@@ -3,6 +3,4 @@ print("hello world")
 print('my name is Lisa')
 
 print("My new edits are here")
-print('My teammate edited the file here like this')
-
-print('push changes')
+print("I edited the file like this")
