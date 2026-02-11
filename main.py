9f34db79919905ad89286f6d67437f57ad1b4bba
lmraine <55120024+lmraine@users.noreply.github.com>
2026-02-11 09:13:51 -0600

    accept incoming change

--- main.py
+++ main.py
@@ -7,8 +7,4 @@ print('My teammate edited the file here like this')
 
 print("Take 2: Teammate did this")
 
-<<<<<<< HEAD
 print("I did this to test vs code")
-=======
-print('Take 3: Test merge of VS Code')
->>>>>>> 7b845348a4f4cc11ce1ac560b66f1012a985f027
